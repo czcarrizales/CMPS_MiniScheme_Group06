8ac00b0bed9fc782890aadba3600129b7f286a35
Chazz Carrizales <czcarrizales@yahoo.com>
2026-03-25 16:48:05 -0700

    deleted starter file for Lisp

--- functional/file-loader-stub.lisp
+++
@@ -1,6 +0,0 @@
-;;; Tiny file-loading helper stub for the functional implementation.
-(defun read-file-as-string (path)
-  (with-open-file (in path :direction :input)
-    (let ((contents (make-string (file-length in))))
-      (read-sequence contents in)
-      contents)))
